d384d9fcfc55dcd41e708279b8f18edd3ad39a72
shatyaki_dutt@hotmail.com <shatyaki_dutt@hotmail.com>
2018-07-19 19:25:58 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const passport = require("passport");
 const app = express();
-
+// Load Routes
+const auth = require("./routes/auth")
+// Passport config
+require("./config/passport")(passport)
+// 
 app.get("/", (req, res) => {
   res.send(`It works`);
 });
+// Use Routes
+app.use("/auth",auth);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
