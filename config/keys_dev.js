4a48db34e9594852f162ee4bdbbb05fc4c75f191
shatyaki_dutt@hotmail.com <shatyaki_dutt@hotmail.com>
2018-07-23 19:21:51 +0530

    Version 1.0

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: "mongodb://Shatyaki:Shatyak2@ds141621.mlab.com:41621/books-dev",
+  googleClientID:
+    "478315438220-8jhvtor0bd14j5qq41urm9bed110h6ms.apps.googleusercontent.com",
+  googleClientSecret: "QOclUQ6fEtIrQ4p1tTHl6B8S"
+};
